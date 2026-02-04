602619e2bc4a8a3c64654a913bd7211a81817376
Ita-Hloks <115126342+Ita-Hloks@users.noreply.github.com>
2026-02-04 12:17:43 +0800

    Refactor: Better UI (#7)
    * Feat: Software List
    
    * FEAT: Software relic
    
    * Feat: Better UI and real time log.

--- Cyclophiops/Form1.Designer.cs
+++ Cyclophiops/Form1.Designer.cs
@@ -17,6 +17,7 @@ namespace Cyclophiops
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
@@ -29,98 +30,124 @@ namespace Cyclophiops
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button1.Location = new System.Drawing.Point(24, 110);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(20, 20);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 46);
+            this.button1.Size = new System.Drawing.Size(180, 36);
             this.button1.TabIndex = 0;
-            this.button1.Text = "BasicInfo";
+            this.button1.Text = "Basic Information";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
-            this.textBox1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(24, 8);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.textBox1.Size = new System.Drawing.Size(526, 53);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
-            // 
             // button2
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button2.Location = new System.Drawing.Point(228, 110);
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(210, 20);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(122, 45);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "SoftwareList";
+            this.button2.Size = new System.Drawing.Size(180, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Installed Software";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // button3
             // 
-            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button3.Location = new System.Drawing.Point(428, 111);
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Location = new System.Drawing.Point(400, 20);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(122, 45);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "HardwareList";
+            this.button3.Size = new System.Drawing.Size(180, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hardware Information";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // button4
             // 
-            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button4.Location = new System.Drawing.Point(24, 175);
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Location = new System.Drawing.Point(20, 66);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(122, 45);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "BrowserHistory";
+            this.button4.Size = new System.Drawing.Size(180, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Browser History";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.Button4_Click);
             // 
             // button5
             // 
-            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button5.Location = new System.Drawing.Point(228, 175);
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button5.Location = new System.Drawing.Point(210, 66);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(122, 45);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "SoftwareResiduals";
+            this.button5.Size = new System.Drawing.Size(180, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Software Residuals";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button6.Location = new System.Drawing.Point(400, 66);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(180, 36);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Run All Tasks";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_ClickAsync);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.Color.White;
+            this.logTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logTextBox.Font = new System.Drawing.Font("Consolas", 9F);
+            this.logTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.logTextBox.Location = new System.Drawing.Point(20, 120);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(560, 250);
+            this.logTextBox.TabIndex = 7;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.statusLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.statusLabel.Location = new System.Drawing.Point(20, 385);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(50, 20);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Ready";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(582, 384);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(611, 431);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.MaximumSize = new System.Drawing.Size(810, 550);
-            this.MinimumSize = new System.Drawing.Size(600, 350);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cyclophiops";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -131,11 +158,12 @@ namespace Cyclophiops
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
-
